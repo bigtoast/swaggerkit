1e7d522a303dd9ab0ab20a3c8bbe8c0f933fdb0d
Andrew Headrick <andrew.headrick@gmail.com>
2013-03-09 13:51:05 -0800

    updates

--- core/src/main/scala/net/eamelink/swaggerkit/SchemaBuilder.scala
+++ core/src/main/scala/net/eamelink/swaggerkit/SchemaBuilder.scala
@@ -1,5 +1,8 @@
 package net.eamelink.swaggerkit
 
+
+import scala.language.implicitConversions 
+
 /**
  * Trait with methods for building Schemas
  */
--- project/Build.scala
+++ project/Build.scala
@@ -5,8 +5,9 @@ object Settings {
   val name = "swaggerkit"
   
   val buildSettings = Project.defaultSettings ++ Seq(
-    version := "0.1",
-    scalaVersion := "2.9.1")
+    version := "0.1-RC3",
+    scalaVersion := "2.10.0",
+    scalacOptions += "-feature")
 }
 
 object Resolvers {
@@ -14,8 +15,8 @@ object Resolvers {
 }
 
 object Dependencies {
-  lazy val play = "play" %% "play" % "2.0"
-  lazy val specs = "org.specs2" %% "specs2" % "1.9" % "test"
+  lazy val play = "play" %% "play" % "2.1-RC3"
+  lazy val specs = "org.specs2" %% "specs2" % "1.13" % "test"
 }
 
 object ApplicationBuild extends Build {
@@ -23,15 +24,22 @@ object ApplicationBuild extends Build {
   import Resolvers._
   import Dependencies._
 
+  lazy val pubTo = Some(Resolver.file("bigtoast.github.com", file(Path.userHome + "/Projects/BigToast/bigtoast.github.com/repo")) ) 
+
   lazy val root = Project(name, file("."), settings = buildSettings ++ Seq(
-    publish := {})
+    //publish := {}
+    publishTo := pubTo )
   ) aggregate (core, play2)
 
   lazy val core = Project(name + "-core", file("core"), settings = buildSettings ++ Seq(
     resolvers := Seq(typesafeRepo),
-    libraryDependencies ++= Seq(specs)))
+    libraryDependencies ++= Seq(specs),
+    publishTo := pubTo  ))
 
   lazy val play2 = Project(name + "-play2", file("play2"), settings = buildSettings ++ Seq(
     resolvers := Seq(typesafeRepo),
-    libraryDependencies ++= Seq(play, specs))) dependsOn (core)
+    libraryDependencies ++= Seq(play, specs),
+    publishTo := pubTo  )) dependsOn (core)
 }
+
+
